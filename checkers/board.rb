563b4694f47c269b954e8d167b046eb57f000f56
Mikey Shean <mdshean2@gmail.com>
2015-08-06 10:26:29 -0400

    Add Board#populate_grid.

--- checkers/board.rb
+++ checkers/board.rb
@@ -1,10 +1,13 @@
+require_relative 'piece'
 
-def Board
+class Board
 
   BOARD_SIZE = 8
 
+  attr_reader :grid
+
   def initialize(fillboard = true)
-    @grid = populate_grid(fillboard)
+    populate_grid(fillboard)
   end
 
   def [](pos)
@@ -17,7 +20,24 @@ def Board
     @grid[row][col] = value
   end
 
-    
+  def populate_grid(fillboard)
+    @grid = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE) }
+    return unless fillboard
 
+    [:red, :black].each do |color|
+      [0,1,2].each do |row_delta|
+        row = (color == :red ? 5 : 0)
+        row += row_delta
+        (0...BOARD_SIZE).each do |col|
+          case (row % 2)
+          when 0
+            self[[row, col]] = Piece.new(color, [row, col]) if col % 2 == 1
+          when 1
+            self[[row, col]] = Piece.new(color, [row, col]) if col % 2 == 0
+          end
+        end
+      end
+    end
+  end
 
 end
