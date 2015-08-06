a184b8e899dd8112bd37b10f60cc51d87ec0f614
Mikey Shean <mdshean2@gmail.com>
2015-08-06 15:24:53 -0400

    Add jumped_pos ivar to set square to nil in Piece#perform_jump => Piece#update_positons.

--- checkers/piece.rb
+++ checkers/piece.rb
@@ -5,7 +5,7 @@ require 'byebug'
 class Piece
 
   attr_reader :color, :board
-  attr_accessor :pos
+  attr_accessor :pos, :jumped_pos
 
   def initialize(color, board, pos)
     @color, @board, @pos = color, board, pos
@@ -29,7 +29,7 @@ class Piece
 
   def perform_jump(new_pos)
     return false if !valid_jump?(new_pos)
-    update_positions(new_pos)
+    update_positions(new_pos, jumped_pos)
     true
   end
 
@@ -41,7 +41,7 @@ class Piece
       dx, dy = delta
       row, col = pos
 
-      jumped_pos = [row + dx, col + dy]
+      @jumped_pos = [row + dx, col + dy]
       landed_pos = [row + (2 * dx), col + (2 * dy)]
 
       return true if jumpable_piece?(jumped_pos, landed_pos)
@@ -88,10 +88,12 @@ class Piece
     end
   end
 
-  def update_positions(new_pos)
+  def update_positions(new_pos, jumped_pos = nil)
     board[self.pos] = nil
     board[new_pos] = self
     self.pos = new_pos
+
+    board[jumped_pos] = nil unless jumped_pos.nil?
     nil
   end
 
