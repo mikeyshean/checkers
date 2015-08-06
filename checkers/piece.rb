443b21573d2968ebad452a4c4bb735f9fb22d183
Mikey Shean <mdshean2@gmail.com>
2015-08-06 10:52:14 -0400

    Add Piece#king? and Piece#move_diffs(returns deltas for color)

--- checkers/piece.rb
+++ checkers/piece.rb
@@ -5,12 +5,22 @@ class Piece
 
   def initialize(color, board, pos)
     @color, @board, @pos = color, board, pos
+    @king = false
     board.add_piece(self, pos)
   end
 
   def to_s
     color.to_s
   end
+
+  def king?
+    @king
+  end
+
+  def move_diffs
+    color == :red ? [[-1, 1], [-1, -1]] : [[1, 1], [1, -1]]
+  end
+
 
 
 end
